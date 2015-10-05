0453093ed1b92a96cf2ef85c6bb4fc937a17585a
Fourat ZOUARI <fourat@gmail.com>
2015-10-05 13:22:36 +0100

    Fixed rpmbuild paths

--- create-rpm.sh
+++ create-rpm.sh
@@ -38,7 +38,7 @@ echo "FROM centos:7" > $DOCKER_FILE
 echo "VOLUME $WORK_DIR/packages" >> $DOCKER_FILE
 echo "MAINTAINER Fourat ZOUARI <fourat@gmail.com>" >> $DOCKER_FILE
 echo "RUN mkdir -p ~/rpmbuild/{RPMS,SRPMS,BUILD,SOURCES,SPECS}" >> $DOCKER_FILE
-echo "ADD ./python-jasmin.spec /root/rpmbuild/SPECS/python-jasmin.spec" >> $DOCKER_FILE
+echo "ADD ./python-jasmin.spec /rpmbuild/SPECS/python-jasmin.spec" >> $DOCKER_FILE
 echo "RUN yum -y install rpm-build tar python-setuptools gcc bzip2 python-devel python-twisted-core" >> $DOCKER_FILE
 echo "RUN curl 'https://bootstrap.pypa.io/get-pip.py'|python" >> $DOCKER_FILE
 echo "RUN curl '$PYPI_JASMIN_URL' -o ~/rpmbuild/SOURCES/jasmin-$1.tar.gz" >> $DOCKER_FILE
@@ -49,7 +49,7 @@ echo "RUN rpmbuild -ba ~/rpmbuild/SPECS/python-jasmin.spec" >> $DOCKER_FILE
 
 # Create rpm inside a centos 7 container
 docker build --rm=true -t jasmin-centos7-rpm $WORK_DIR/images/centos7
-docker run --rm -v $WORK_DIR/packages:/tmp/packages -it jasmin-centos7-rpm /bin/sh -c 'cp /root/rpmbuild/RPMS/noarch/*.rpm /tmp/packages'
+docker run --rm -v $WORK_DIR/packages:/tmp/packages -it jasmin-centos7-rpm /bin/sh -c 'cp /rpmbuild/RPMS/noarch/*.rpm /tmp/packages'
 docker rmi -f jasmin-centos7-rpm
 
 # Get packages
